f839f893ba8c045cf84cf9c34be24967a292701a
Fernando <faaraya1@uc.cl>
2017-03-14 23:22:15 -0300

    listo para funcionar

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170314035537) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "subhead"
@@ -23,10 +26,11 @@ ActiveRecord::Schema.define(version: 20170314035537) do
   create_table "comments", force: :cascade do |t|
     t.string "username"
     t.text "body"
-    t.integer "article_id"
+    t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "comments", "articles"
 end
